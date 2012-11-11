bebaea6c90b43007ace3a1c8293f1d0baf2fbef4
SmartGeometry <1938304514@qq.com>
2012-11-11 21:43:04 +0800

    the first version

--- SGpad/gen/com/sg/property/R.java
+++ SGpad/gen/com/sg/property/R.java
@@ -29,7 +29,6 @@ public final class R {
         public static final int folder=0x7f02000f;
         public static final int gray=0x7f020010;
         public static final int green=0x7f020011;
-<<<<<<< HEAD
         public static final int ic_launcher=0x7f020012;
         public static final int isoceles_tri=0x7f020013;
         public static final int isosceles_trap=0x7f020014;
@@ -91,71 +90,6 @@ public final class R {
         public static final int popup_for_rect=0x7f030002;
         public static final int popup_for_tri=0x7f030003;
         public static final int popup_for_tri_constrain=0x7f030004;
-=======
-        public static final int home=0x7f020012;
-        public static final int ic_launcher=0x7f020013;
-        public static final int isoceles_tri=0x7f020014;
-        public static final int isosceles_trap=0x7f020015;
-        public static final int midline=0x7f020016;
-        public static final int minimum=0x7f020017;
-        public static final int open=0x7f020018;
-        public static final int parallelogram=0x7f020019;
-        public static final int pause=0x7f02001a;
-        public static final int pen=0x7f02001b;
-        public static final int pencolor=0x7f02001c;
-        public static final int penstyle=0x7f02001d;
-        public static final int pink=0x7f02001e;
-        public static final int play=0x7f02001f;
-        public static final int purple=0x7f020020;
-        public static final int record=0x7f020021;
-        public static final int recorder=0x7f020022;
-        public static final int rect=0x7f020023;
-        public static final int red=0x7f020024;
-        public static final int redo=0x7f020025;
-        public static final int right_angled_isosceles_tri=0x7f020026;
-        public static final int right_angled_tri=0x7f020027;
-        public static final int right_trap=0x7f020028;
-        public static final int save=0x7f020029;
-        public static final int solid=0x7f02002a;
-        public static final int squre=0x7f02002b;
-        public static final int stop=0x7f02002c;
-        public static final int trapezoid=0x7f02002d;
-        public static final int undo=0x7f02002e;
-        public static final int vertical_line=0x7f02002f;
-        public static final int white=0x7f020030;
-        public static final int yellow=0x7f020031;
-    }
-    public static final class id {
-        public static final int angle_bisector=0x7f06000e;
-        public static final int command_line=0x7f06000f;
-        public static final int diamond=0x7f060008;
-        public static final int editName=0x7f060015;
-        public static final int equilateral_tri=0x7f060013;
-        public static final int file_name=0x7f060001;
-        public static final int icon=0x7f060000;
-        public static final int isoceles_tri=0x7f060010;
-        public static final int isosceles_trap=0x7f06000b;
-        public static final int list=0x7f060003;
-        public static final int midline=0x7f06000d;
-        public static final int parallelogram=0x7f060005;
-        public static final int parent=0x7f060004;
-        public static final int path=0x7f060002;
-        public static final int rect=0x7f060006;
-        public static final int right_angled_isosceles_tri=0x7f060012;
-        public static final int right_angled_tri=0x7f060011;
-        public static final int right_trap=0x7f06000a;
-        public static final int saveForm=0x7f060014;
-        public static final int squre=0x7f060007;
-        public static final int trapezoid=0x7f060009;
-        public static final int vertical_line=0x7f06000c;
-    }
-    public static final class layout {
-        public static final int line=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int popup_for_rect=0x7f030002;
-        public static final int popup_for_restriction=0x7f030003;
-        public static final int popup_for_tri=0x7f030004;
->>>>>>> 6b2cc19dd2b5dbb813a83dbcce80ed911b60da12
         public static final int save=0x7f030005;
     }
     public static final class raw {
